ac794f1fff12c3e11a39a8b4a73b0d432da9a745
Christopher Wong <cw30355@gmail.com>
2023-01-12 00:17:25 -0500

    commit tree 370c09e26f1fd348536c84efc878d5299ae53c10

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+/*const main = document.getElementById("main")*/
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Chris is the champion"
+
+document.body.append(newHeader);
+/* why does this have to be at the end? */
